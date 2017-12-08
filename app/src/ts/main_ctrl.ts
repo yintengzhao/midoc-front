fc28f6cbabb9827c772954710cd749ab0d9b3023
darfux <darfux@163.com>
2017-12-08 20:49:09 +0800

    main map prototype

--- app/src/ts/main_ctrl.ts
+++ app/src/ts/main_ctrl.ts
@@ -2,8 +2,29 @@ import {ng_app} from './ng_app';
 
 ng_app.controller("MainCtrl", ['$scope', '$interval', '$timeout', '$window', '$http', '$sce',
 function($scope, $interval, $timeout, $window, $http, $sce){
-  $scope.liuliu = 667
-  $('#some-nixie-html-value-id').nixie({ value: '66:66:66' });
+  $scope.liuliu = 667;
+
+  $scope.map = {
+    slider: 100,
+  };
+
+  $scope.map_header = {
+    total_m: 5000,
+    total_v: 7000,
+    platform: 3,
+    ship: 5,
+  };
+
+  $scope.handle_click_on_map = function(event) {
+    console.log(event.offsetX, event.offsetY);
+  };
+
+  $scope.prevent_bubble = function(event) {
+    console.log(event);
+    event.stopPropagation();
+  }
+
+
 }])
 
 angular.bootstrap(document.getElementsByTagName("body")[0], ['fmid']);
