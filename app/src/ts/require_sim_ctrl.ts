37bf184dc924bf4b075c6831757b98a19ee1c62e
zhaowenjian <zhaowenjian@github.com>
2018-01-28 22:22:09 +0800

    2018/1/28 22.22

--- app/src/ts/require_sim_ctrl.ts
+++ app/src/ts/require_sim_ctrl.ts
@@ -4,4 +4,30 @@ import { ng_app } from './ng_app';
 ng_app.controller("RequireSimCtrl", ['$scope', '$interval', '$timeout', '$window', '$http', '$sce',
   function($scope, $interval, $timeout, $window, $http, $sce) {
 
+    $scope.labels = ["1", "2", "3", "4", "5", "6", "7","8", "9", "10"];
+  $scope.series = ['实际需求量', '预测需求量'];
+  $scope.data = [
+    [75, 79, 80, 81, 85, 88, 90,80,79,70],
+    [70, 75, 78, 83, 80, 80, 92,85,80,73]
+  ];
+
+
+  $scope.confirmFunction=function(){
+    $scope.data[0].shift();
+    $scope.data[0].push($scope.aa)
+  }
+
+  $scope.abc=true;
+
+  $scope.yes=function(){
+    $scope.abc=false;
+  }
+
+  $scope.modifydata=function(){
+    $scope.abc=true;
+  }
+
+
+
+
 }
